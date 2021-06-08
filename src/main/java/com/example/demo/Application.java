50808ab8435ab91c8ebed476a9f39627bf564093
priya yenurkar <yenurkarpriya22@gmail.com>
2021-06-08 22:45:25 +0530

    HIS-301:related changeds

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,11 +6,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+        private Map<String,Object> cache= new HashMap<String, Object>();
 	public static void main(String[] args) {
-	  ConfigurableApplicationContext run	=SpringApplication.run(Application.class, args);
+	  ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
             int a=20;
 	run.close();
 	}
-
+  //HIS-301 changes
+   public void loadDataToCache(){
+    //logic
+   }
 }
