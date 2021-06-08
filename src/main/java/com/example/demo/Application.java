8dc17b1b94d723812512956dfaa54a92e2192fa6
priya yenurkar <yenurkarpriya22@gmail.com>
2021-06-08 22:18:56 +0530

    HIS-300:related changers

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,7 +6,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+        
+       private static final String REST_URL="https://www.equifax.com/getscore";
 	public static void main(String[] args) {
 	  ConfigurableApplicationContext run	=SpringApplication.run(Application.class, args);
             int a=20;
